28bc4cb2cfb557dc7b36f23ac7968a25e2aad3b3
Matheus Goulart Leite da Fonseca <ra2457097>
2026-05-15 21:29:16 -0300

    Aula 15/05/2026

--- app/Http/Controllers/Admin/CursoController.php
+++ app/Http/Controllers/Admin/CursoController.php
@@ -11,8 +11,28 @@ class CursoController extends Controller
     //
     public function index() {
         $rows = Curso::all();
-        return view('admin.cursos.index', compact('rows'));
+        return view('Admin.cursos.index', compact('rows'));
     }
+
+    public function adicionar() {
+    return view('admin.cursos.adicionar');
+    }
+    public function editar($id) {
+    // repare que ele recebe o id da ROTA
+    $linha = Curso::find($id);
+    // carrega o registro (realiza um select e um fetch internamente)
+    return view('admin.cursos.editar',compact('linha'));
+    // manda o registro encontrado para ser editado na visão
+    }
+    public function excluir($id) {
+    // repare que ele recebe o id da ROTA
+    Curso::find($id)->delete();
+    // apos selecionar o registro, é chamado o
+    // método DELETE do OBJETO registro
+    return redirect()->route('admin.cursos');
+    // abre a visão da lista de cursos
+}
+
 }
 
 
